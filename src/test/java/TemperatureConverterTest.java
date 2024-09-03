51d451d44db0f8528294f6c2c14bacdd26ed12f7
Jannassl <Jan.nassling@metropolia.fi>
2024-09-03 09:51:55 +0300

    Tehtäviä

---
+++ src/test/java/TemperatureConverterTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TemperatureConverterTest {
+
+    @Test
+    public void fahrenheitToCelsius() {
+    }
+
+    @Test
+    public void celsiusToFahrenheit() {
+    }
+
+    @Test
+    public void isExtremeTemperature() {
+    }
+}
